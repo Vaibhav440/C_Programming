c7a8b67f12f51751d8ab5562108d2f70de81f921
Vaibhav440 <128299280+Vaibhav440@users.noreply.github.com>
2023-05-19 08:18:32 +0530

    Add files via upload

---
+++ Assignment_16/2.c
@@ -0,0 +1,36 @@
+//Problem Statement-:Accept number of rows and number of columns from user and display below pattern.
+
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int i=0,j=0;
+    char ch='\0';
+    char ch1='\0';
+    for(i=1;i<=iRow;i++)
+    {
+        for(j=1,ch='A',ch1='a';j<=iCol;j++,ch++,ch1++)
+        {
+          if((i%2)==0)
+          {
+            printf("%c\t",ch1);
+          }
+          else
+          {
+            printf("%c\t",ch);
+          }
+        }
+         printf("\n");
+    }
+    
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+    printf("Enter the size of Row::\n");
+    scanf("%d",&iValue1);
+    printf("Enter the size of Column::\n");
+    scanf("%d",&iValue2);
+    Pattern(iValue1,iValue2);
+    return 0;
+}
