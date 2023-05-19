87624fd3ade01b98f3aeb164b2b23f566a317b5e
Vaibhav440 <128299280+Vaibhav440@users.noreply.github.com>
2023-05-19 08:17:48 +0530

    Add files via upload

---
+++ Assignment_15/1.c
@@ -0,0 +1,27 @@
+//Accept number of rows and number of columns from user and display below pattern
+
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int i=0,j=0;
+    for(i=1;i<=iRow;i++)
+    {
+        for(j=1;j<=iCol;j++)
+        {
+            printf("*\t",j);
+        }
+         printf("\n");
+    }
+    
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+    printf("Enter the size of Row::\n");
+    scanf("%d",&iValue1);
+    printf("Enter the size of Column::\n");
+    scanf("%d",&iValue2);
+    Pattern(iValue1,iValue2);
+    return 0;
+}
